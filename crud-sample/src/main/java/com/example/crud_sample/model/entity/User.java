daee26b6d54027c2b244805c3a94b041c3d8890a
Pial Kanti Samadder <pialkanti2012@gmail.com>
2025-10-23 16:04:36 +0600

    Implemented User Entity class

--- crud-sample/src/main/java/com/example/crud_sample/model/entity/User.java
+++ crud-sample/src/main/java/com/example/crud_sample/model/entity/User.java
@@ -1,14 +1,27 @@
 package com.example.crud_sample.model.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 100)
     private String firstName;
+
+    @Column(name = "last_name", length = 50)
     private String lastName;
+
+    @Column(name = "email", nullable = false, length = 100)
     private String email;
+
+    @Column(name = "username", nullable = false, length = 50)
     private String username;
 }
