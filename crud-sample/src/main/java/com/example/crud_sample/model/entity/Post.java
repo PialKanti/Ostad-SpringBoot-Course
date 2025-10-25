3f31a1746ba7aa18b42677c216ebd7e83ff2675d
Pial Kanti Samadder <pialkanti2012@gmail.com>
2025-10-25 21:55:54 +0600

    Removed user object from Post response

--- crud-sample/src/main/java/com/example/crud_sample/model/entity/Post.java
+++ crud-sample/src/main/java/com/example/crud_sample/model/entity/Post.java
@@ -1,5 +1,6 @@
 package com.example.crud_sample.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,5 +20,6 @@ public class Post {
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
+    @JsonIgnore
     private User user;
 }
